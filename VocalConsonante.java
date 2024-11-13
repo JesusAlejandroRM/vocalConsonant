337f86ca57b0e4e85c986e9847965e186e6057ed
JesusAlejandroRM <josealejandrorosiles@gmail.com>
2024-11-12 19:20:23 -0600

    Update VocalConsonante.java

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -23,16 +23,20 @@ class Vocales{
     public boolean vocal(){
         boolean flag = false;
         char c;
-        String v = "Es vocal", nv = "Es consonante";
+        String v = " es vocal", nv = " es consonante";
         for(int i = 0; i < (str1.length()); i++){
             c = str1.charAt(i);
             if(c == 'a' || c == 'e'|| c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E'|| c == 'I' || c == 'O' || c == 'U' || c == 'á' || c == 'é' || c == 'í' || c == 'ó' || c == 'ú' || c == 'Á' || c == 'É' || c == 'Í' || c == 'Ó' || c == 'Ú'){
-                System.out.println(v);
+                System.out.println(c + v);
                 flag = true;
             }
             else{
-                System.out.println(nv);
-                flag = false;
+                System.out.println(c + nv);
+                if(c == 'p' || c == 'P'){
+                    flag = false;}
+                else{
+                    flag = true;
+                }
             }
         }
         return flag;
