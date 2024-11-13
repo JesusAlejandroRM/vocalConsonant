3a2ee893244924b49195e236f2c5282563f7c0bc
Jesus <josealejandrorosiles@gmail.com>
2024-11-12 19:24:18 -0600

    El programa inicialmete recibia una frase para retornar cuantas consonantes o vocales contenia, con la modificacion hecha, solamente retorna si es vocal o consonante, parando hasta que el usuario tecle la letra q

---
+++ VocalConsonante.java
@@ -0,0 +1,40 @@
+package com.mycompany.vocalconsonante;
+import javax.swing.JOptionPane;
+
+public class VocalConsonante {
+
+    public static void main(String[] args) {
+        String str1 = JOptionPane.showInputDialog(null, "Ingresa tu frase: ");    
+        System.out.println("Longitud de la cadena: " + str1.length());//.lenght sirve para conocer la longitud de la cadena
+        Vocales v = new Vocales(str1);
+        boolean esVocal = v.vocal();
+        while(esVocal){
+            str1 = JOptionPane.showInputDialog(null, "Ingresa tu frase: ");
+            v = new Vocales(str1);
+            esVocal = v.vocal();
+        }
+    }
+}
+class Vocales{
+    String str1;
+    public Vocales(String str1){
+        this.str1=str1;
+    }
+    public boolean vocal(){
+        boolean flag = false;
+        char c;
+        String v = "Es vocal", nv = "Es consonante";
+        for(int i = 0; i < (str1.length()); i++){
+            c = str1.charAt(i);
+            if(c == 'a' || c == 'e'|| c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E'|| c == 'I' || c == 'O' || c == 'U' || c == 'á' || c == 'é' || c == 'í' || c == 'ó' || c == 'ú' || c == 'Á' || c == 'É' || c == 'Í' || c == 'Ó' || c == 'Ú'){
+                System.out.println(v);
+                flag = true;
+            }
+            else{
+                System.out.println(nv);
+                flag = false;
+            }
+        }
+        return flag;
+    }
+}
